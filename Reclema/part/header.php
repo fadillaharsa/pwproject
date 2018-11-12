a3ec7c7a03ff2b86919d30ff7bfcc54b0be9e67c
fadillaharsa <arsabandung@gmail.com>
2018-11-13 02:52:57 +0700

    ganti logo

--- Reclema/part/header.php
+++ Reclema/part/header.php
@@ -2,7 +2,7 @@
 <div class="container-fluid">
 	<div class="row" style="background: #00BFFF; color: white">
 		<div class="col-sm-2">
-			<a href="home.php"><img src="content/reclema-full.png" width="200px" style="margin-top:10px; margin-bottom: 10px"></a>
+			<a href="home.php"><img src="content/reclema logo.png" width="200px" style="margin-top:10px; margin-bottom: 10px"></a>
 		</div>
 		<div class="col-sm-7">
 			<p style="margin-top:15px; margin-bottom: 10px"><i>Rekrutmen Lembaga Mahasiswa Universitas Padjadjaran</i></p>
