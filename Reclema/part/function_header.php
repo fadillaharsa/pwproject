e8d10d87fc158ec0ad86eedeed51eadd91a3f0db
fadillaharsa <arsabandung@gmail.com>
2018-11-13 14:06:30 +0700

    tambah css function header

--- Reclema/part/function_header.php
+++ Reclema/part/function_header.php
@@ -2,17 +2,17 @@
 	<div class="bentuk">
 		<form style="padding-top: 10px; padding-bottom: 10px">
 			<input class="rcorners1" type="text" name="pencarian" placeholder="Cari rekrutmen">
-			<select name="kategori" class="rcorners1">
+			<select type="select" name="kategori" class="rcorners2">
 				<option value="Kepanitiaan">Kepanitiaan</option>
 				<option value="UKM">UKM</option>
 				<option value="BE">BE</option>					
 			</select>
-			<select name="lingkup" class="rcorners1">
+			<select type="select" name="lingkup" class="rcorners2">
 				<option value="Universitas">Universitas</option>
 				<option value="Fakultas">Fakultas</option>
 				<option value="Jurusan">Jurusan</option>					
 			</select>
-			<input type="button" value="Cari" class="tombol1">
+			<input type="button" value="Cari" class="tombolcari">
 		</form>
 	</div>
 </div>	
