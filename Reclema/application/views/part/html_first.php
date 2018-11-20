70491a4affff6213cbd5810a868c3044b04a393e
fadillaharsa <arsabandung@gmail.com>
2018-11-20 15:29:43 +0700

    memisahkan file js

--- Reclema/application/views/part/html_first.php
+++ Reclema/application/views/part/html_first.php
@@ -11,14 +11,10 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+	<script src="../stylesheet/ourscript.js"></script>
 
 	<script>
-	    $(document).ready(function(){
-	      $('.slider').bxSlider({
-	      	auto: true,
-  			pager: true,
-	      });
-	    });
+
 	</script>
 
 	<style>
