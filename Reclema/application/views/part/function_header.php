de6138ef5dfe300791bb82459c390c3bc04d3220
fadillaharsa <arsabandung@gmail.com>
2018-11-20 15:20:46 +0700

    memisahkan file css, header jadi, header function jadi

--- Reclema/application/views/part/function_header.php
+++ Reclema/application/views/part/function_header.php
@@ -1,18 +1,20 @@
 <div class="container-fluid" style="background: #00aad4ff; color: white; border-bottom: 1px solid #A9A9A9">
 	<div>
 		<form style="padding-top: 10px; padding-bottom: 10px">
-			<input class="rcorners1" type="text" name="pencarian" placeholder="Cari rekrutmen">
+			<input type="search" name="pencarian" placeholder="Cari rekrutmen" class="rcorners1">
 			<select type="select" name="kategori" class="rcorners2">
+				<option value="">-- Kategori</option>
 				<option value="Kepanitiaan">Kepanitiaan</option>
 				<option value="UKM">UKM</option>
 				<option value="BE">BE</option>					
 			</select>
 			<select type="select" name="lingkup" class="rcorners2">
+				<option value="">-- Lingkup</option>
 				<option value="Universitas">Universitas</option>
 				<option value="Fakultas">Fakultas</option>
 				<option value="Jurusan">Jurusan</option>					
 			</select>
-			<input type="button" value="Cari" class="tombolcari">
+			<input type="submit" value="Cari" class="tombolcari">
 		</form>
 	</div>
 </div>	
