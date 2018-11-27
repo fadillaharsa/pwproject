41a86ddb410f3602eb929592354b19a900d1ae3d
fadillaharsa <arsabandung@gmail.com>
2018-11-28 05:04:03 +0700

    edit footer css

--- Reclema/application/views/part/footer.php
+++ Reclema/application/views/part/footer.php
@@ -1,18 +1,18 @@
-<div class="container-fluid">
-	<div class="row" style="background: #333333ff; color: white">
-		<div class="col-sm-4"><u>Tentang Kami</u>
-			<img src="">
-			<h1>RECLEMA</h1>
+<div class="container-fluid" style="margin-top: 10px">
+	<div class="row" style="background: #333333ff; color: white; padding-top:20px" >
+		<div class="col-sm-4">
+			<a href="../index.php/home"><img src="../content/reclema logo.png" width="200px" style="margin-bottom: 10px"></a>
+			<p>Reclema adalah web perekrutan keanggotaan lembaga kemahasiswaan yang berfungsi intuk memudahkan lembaga kemahasiswaan/kepanitiaan dalam merekrut anggota barunya.</p>
 		</div>
 		<div class="col-sm-4">
-			<h4>TAUTAN META</h4>
+			<h4 style="color: white">TAUTAN META</h4>
 			<ul>
 				<li><a href="https://paus.unpad.ac.id/">Universitas Padjadjaran</a></li>
 				<li><a href="http://himatif.fmipa.unpad.ac.id/">Himatif FMIPA Unpad</a></li>	
 			</ul>
 		</div>
 		<div class="col-sm-4">
-			<h4>LAINNYA</h4>
+			<h4 style="color: white">LAINNYA</h4>
 			<ul>
 				<li><a href="https://www.google.com">Bantuan</a></li>
 				<li><a href="https://www.google.co.id">Ketentuan Penggunaan</a></li>
