1d170d7b3134ede59ea33fa209f96d617d190d04
JohnLtor19 <johannes17001@mail.unpad.ac.id>
2018-12-06 00:39:11 +0700

    Profil Mahasiswa

---
+++ Reclema/application/controllers/Profil_mahasiswa.php
@@ -0,0 +1,6 @@
+<?php
+class Profil_mahasiswa extends CI_Controller {
+	function index() {
+		$this->load->view("profil_mahasiswa");
+	}
+}
---
+++ Reclema/application/views/profil_mahasiswa.php
@@ -0,0 +1,36 @@
+<?php
+include "part/html_first.php";
+include "part/header.php";
+include "part/function_header.php";
+?>
+
+<!--MAIN-->
+<div class="sidebar">
+	<ul>
+		<li><a href="#profilsaya">Profil Saya</a></li>
+		<li><a href="#profillembaga">Lembaga Kemahasiswaan yang saya daftar</a></li>
+	</ul>
+</div>
+<div class="isi">
+	<h1>Profil Saya</h1><br>
+	<img src="../content/Foto SBMPTN.jpg" width="200px" height="300px" align="left">
+	<p align="left">
+	Nama<span style="white-space: pre;">			</span>: Johannes Lumbantoruan<br>
+	NPM<span style="white-space: pre;">			</span>: 140810170043<br>
+	Email<span style="white-space: pre;">			</span>: johannestrikardo@gmail.com<br>
+	Jenis Kelamin<span style="white-space: pre;">	</span>: Laki-laki<br>
+	Password<span style="white-space: pre;">		</span>: ********<br>
+	No. HP<span style="white-space: pre;">			</span>: 08225828XXXX<br>
+	Alamat<span style="white-space: pre;">			</span>: Pondok Sakinah 1<br>
+	Angkatan<span style="white-space: pre;">		</span>: 2017<br>
+	Fakultas<span style="white-space: pre;">			</span>: Fakultas Pengetahuan Alam dan Matematika (FMIPA)<br>
+	Prodi<span style="white-space: pre;">			</span>: Teknik Informatika<br>
+	Kontak lain<span style="white-space: pre;">			</span>:<br><br>
+	</p>
+	<input class="tomboledit" type="button" name="Edit" value="Edit">
+</div>
+<!--FOOTER-->
+<?php
+include "part/footer.php";
+include "part/html_last.php";
+?>
