2fa574b679e9b35f2061adac3f7187ea210a2002
JohnLtor19 <johannes17001@mail.unpad.ac.id>
2018-12-07 23:34:37 +0700

    Ngubah halaman yang tadi

--- Reclema/application/views/profil_lembaga.php
+++ Reclema/application/views/profil_lembaga.php
@@ -5,33 +5,37 @@ include "part/function_header.php";
 ?>
 
 <!--MAIN-->
-<div class="sidebar">
-	<ul>
-		<li><a href="#profilsaya">Profil Saya</a></li>
-		<li><a href="#profillembaga">Lembaga Kemahasiswaan yang saya daftar</a></li>
-	</ul>
-</div>
-<div class="isi">
-	<h1>Lembaga Kemahasiswaan yang saya daftar</h1>	
-	<table border="1px solid black" width="auto">
-		<tr>
-			<th>No</th>
-			<th>Logo Lembaga</th>
-			<th>Lembaga yang di daftar</th>
-		</tr>
-		<tr>
-			<td>1. </td>
-			<td><img src="../content/himatif.png" width="100px" height="150px" align="left"></td>
-			<td>
-				<p align="left">
-				Nama Lembaga<span style="white-space: pre;">			</span>: Himpunan Mahasiswa Teknik Informatika (HIMATIF)<br>
-				Program Studi<span style="white-space: pre;">			</span>: Teknik Informatika<br>
-				Fakultas<span style="white-space: pre;">					</span>: Fakultas Pengetahuan Alam dan Matematika (FMIPA)
-				<br><br>
-				</p>
-			</td>
-		</tr>
-	</table>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-3" style="background: #008080; color: white">
+			<ul>
+				<li><a href="#profilsaya">Profil Saya</a></li>
+				<li><a href="#profillembaga">Lembaga Kemahasiswaan yang saya daftar</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-9">
+			<h1>Lembaga Kemahasiswaan yang saya daftar</h1>	
+			<table border="1px solid black" width="auto">
+				<tr>
+					<th>No</th>
+					<th>Logo Lembaga</th>
+					<th>Lembaga yang di daftar</th>
+				</tr>
+				<tr>
+					<td>1. </td>
+					<td><img src="../content/himatif.png" width="100px" height="150px" align="left"></td>
+					<td>
+						<p align="left">
+						Nama Lembaga<span style="white-space: pre;">			</span>: Himpunan Mahasiswa Teknik Informatika (HIMATIF)<br>
+						Program Studi<span style="white-space: pre;">			</span>: Teknik Informatika<br>
+						Fakultas<span style="white-space: pre;">					</span>: Fakultas Pengetahuan Alam dan Matematika (FMIPA)
+						<br><br>
+						</p>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
 </div>
 
 <!--FOOTER-->
--- Reclema/application/views/profil_mahasiswa.php
+++ Reclema/application/views/profil_mahasiswa.php
@@ -5,30 +5,35 @@ include "part/function_header.php";
 ?>
 
 <!--MAIN-->
-<div class="sidebar">
-	<ul>
-		<li><a href="#profilsaya">Profil Saya</a></li>
-		<li><a href="#profillembaga">Lembaga Kemahasiswaan yang saya daftar</a></li>
-	</ul>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-3" style="background: #008080; color: white">
+			<ul>
+				<li><a href="#profilsaya">Profil Saya</a></li>
+				<li><a href="#profillembaga">Lembaga Kemahasiswaan yang saya daftar</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-9">
+			<h1>Profil Saya</h1><br>
+			<img src="../content/Foto SBMPTN.jpg" width="200px" height="300px" align="left">
+			<p align="left">
+			Nama<span style="white-space: pre;">			</span>: Johannes Lumbantoruan<br>
+			NPM<span style="white-space: pre;">			</span>: 140810170043<br>
+			Email<span style="white-space: pre;">			</span>: johannestrikardo@gmail.com<br>
+			Jenis Kelamin<span style="white-space: pre;">	</span>: Laki-laki<br>
+			Password<span style="white-space: pre;">		</span>: ********<br>
+			No. HP<span style="white-space: pre;">			</span>: 08225828XXXX<br>
+			Alamat<span style="white-space: pre;">			</span>: Pondok Sakinah 1<br>
+			Angkatan<span style="white-space: pre;">		</span>: 2017<br>
+			Fakultas<span style="white-space: pre;">			</span>: Fakultas Pengetahuan Alam dan Matematika (FMIPA)<br>
+			Prodi<span style="white-space: pre;">			</span>: Teknik Informatika<br>
+			Kontak lain<span style="white-space: pre;">			</span>:<br><br>
+			</p>
+			<input class="tomboledit" type="button" name="Edit" value="Edit">
+		</div>
+	</div>
 </div>
-<div class="isi">
-	<h1>Profil Saya</h1><br>
-	<img src="../content/Foto SBMPTN.jpg" width="200px" height="300px" align="left">
-	<p align="left">
-	Nama<span style="white-space: pre;">			</span>: Johannes Lumbantoruan<br>
-	NPM<span style="white-space: pre;">			</span>: 140810170043<br>
-	Email<span style="white-space: pre;">			</span>: johannestrikardo@gmail.com<br>
-	Jenis Kelamin<span style="white-space: pre;">	</span>: Laki-laki<br>
-	Password<span style="white-space: pre;">		</span>: ********<br>
-	No. HP<span style="white-space: pre;">			</span>: 08225828XXXX<br>
-	Alamat<span style="white-space: pre;">			</span>: Pondok Sakinah 1<br>
-	Angkatan<span style="white-space: pre;">		</span>: 2017<br>
-	Fakultas<span style="white-space: pre;">			</span>: Fakultas Pengetahuan Alam dan Matematika (FMIPA)<br>
-	Prodi<span style="white-space: pre;">			</span>: Teknik Informatika<br>
-	Kontak lain<span style="white-space: pre;">			</span>:<br><br>
-	</p>
-	<input class="tomboledit" type="button" name="Edit" value="Edit">
-</div>
+
 <!--FOOTER-->
 <?php
 include "part/footer.php";
