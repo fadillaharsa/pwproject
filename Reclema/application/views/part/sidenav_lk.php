3bff41b36f44714b16820229f34238b7e83c499b
radjfauzi <radjfauzi@gmail.com>
2018-12-07 22:52:30 +0700

    add sidenav_lk

---
+++ Reclema/application/views/part/sidenav_lk.php
@@ -0,0 +1,5 @@
+<div class="col-sm-3" style="background: #008080; color: white"><br>
+				<a style="font-size: 20px" href="https://www.google.com">Profil Lembaga</a><br>
+				<a style="font-size: 20px" href="https://www.google.co.id">Perekrutan</a><br>
+				<a style="font-size: 20px" href="https://www.facebook.com">Pendaftar</a><br>
+</div>
