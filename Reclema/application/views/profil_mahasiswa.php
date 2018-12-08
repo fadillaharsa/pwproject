663493b00f1b4e132a26ce2770ca6f929f730a4e
radjfauzi <radjfauzi@gmail.com>
2018-12-08 22:34:43 +0700

    profil_mahasiswa

--- Reclema/application/views/profil_mahasiswa.php
+++ Reclema/application/views/profil_mahasiswa.php
@@ -11,52 +11,55 @@ include "part/function_header.php";
 		<div class="col-sm-9">
 			<h2>Profil Saya</h2>
 			<table class="table">
-					<tr>
-						<th>Foto</th>
-						<td><img width="100px" src="<?php echo base_url()?>/content/Foto.jpg"></td>
-					</tr>
+				<?php 
+					echo "<tr><th>Foto</th><td>";
 
-					<tr>
-						<th>Nama</th>
-						<td>test</td>
-					</tr>
-					<tr>
-						<th>NPM</th>
-						<td>safsdfasds da acadsdas  sadasdasdsac sada</td>
-					</tr>
-					<tr>
-						<th>Password</th>
-						<td>.</td>
-					</tr>
-					<tr>
-						<th>Email</th>
-						<td>.</td>
-					</tr>
-					<tr>
-						<th>Jenis Kelamin</th>
-						<td>.</td>
-					</tr>
-					<tr>
-						<th>Fakultas</th>
-						<td>.</td>
-					</tr>
-					<tr>
-						<th>Program Studi</th>
-						<td>.</td>
-					</tr>
-					<tr>
-						<th>Alamat</th>
-						<td>.</td>
-					</tr>
-					<tr>
-						<th>No. HP</th>
-						<td>.</td>
-					</tr>
-					<tr>
-						<th>Kontak Lain</th>
-						<td>.</td>
-					</tr>
-			</table>
+					<img width="100px" src="<?php echo base_url()?>/content/Foto.jpg"></td>
+
+					echo "</tr><tr><th>Nama</th><td>";
+					
+					test
+
+					echo "</td></tr><tr><th>NPM</th><td>";
+
+					safsdfasds da acadsdas  sadasdasdsac sada
+
+					echo "</td></tr><tr><th>Password</th><td>";
+					
+					.
+
+					echo "</td></tr><tr><th>Email</th><td>";
+
+					.
+
+					echo "</td></tr><tr><th>Jenis Kelamin</th><td>";
+
+					.
+
+					echo "</td></tr><tr><th>Fakultas</th><td>";
+
+					.
+
+					echo "</td></tr><tr><th>Program Studi</th><td>";
+
+					.
+
+					echo "</td></tr><tr><th>Alamat</th><td>";
+
+					.
+
+					echo "</td></tr><tr><th>No. HP</th><td>";
+
+					.
+
+					echo "</td></tr><tr><th>Kontak Lain</th><td>";
+
+					.
+
+					echo "</td></tr>";
+
+				?>	
+				</table>
 			<button class="tombol tombolwarna2" type="submit">Edit</button><br><br>	
 		</div>
 			
