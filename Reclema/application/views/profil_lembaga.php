70356954ead8f56b3386b18292f868b8c5eb29c3
radjfauzi <radjfauzi@gmail.com>
2018-12-08 17:49:58 +0700

    tambah profil_lembaga

--- Reclema/application/views/profil_lembaga.php
+++ Reclema/application/views/profil_lembaga.php
@@ -7,23 +7,18 @@ include "part/function_header.php";
 <!--MAIN-->
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-sm-3" style="background: #008080; color: white">
-			<ul>
-				<li><a href="#profilsaya">Profil Saya</a></li>
-				<li><a href="#profillembaga">Lembaga Kemahasiswaan yang saya daftar</a></li>
-			</ul>
-		</div>
+		<?php include "part/sidenav_mahasiswa.php"; ?>
 		<div class="col-sm-9">
 			<h1>Lembaga Kemahasiswaan yang saya daftar</h1>	
 			<table border="1px solid black" width="auto">
 				<tr>
 					<th>No</th>
 					<th>Logo Lembaga</th>
-					<th>Lembaga yang di daftar</th>
+					<th style="width: 100%">Lembaga yang di daftar</th>
 				</tr>
 				<tr>
-					<td>1. </td>
-					<td><img src="../content/himatif.png" width="100px" height="150px" align="left"></td>
+					<td style="width: auto;">1. </td>
+					<td style="width: auto;"><img width="100px" src="<?php echo base_url()?>/content/himatif.png"></td>
 					<td>
 						<p align="left">
 						Nama Lembaga<span style="white-space: pre;">			</span>: Himpunan Mahasiswa Teknik Informatika (HIMATIF)<br>
