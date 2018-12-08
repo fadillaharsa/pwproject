6596f6eaf33ec70fa155215ee754d62f67f07484
radjfauzi <radjfauzi@gmail.com>
2018-12-08 16:48:26 +0700

    tambah page lk_info_perekrutan

--- Reclema/application/views/lk_info_perekrutan.php
+++ Reclema/application/views/lk_info_perekrutan.php
@@ -4,43 +4,51 @@ include "part/header.php";
 include "part/function_header.php";
 ?>
 
-<!--MAIN-->
-<div class="container-fluid">	
+<div class="container-fluid">
 	<div class="row">
-		<div class="col-sm-12">
+		<?php include "part/sidenav_lk.php"; ?>
+		<div class="col-sm-9">
 			<h2>Daftar Recruitment Lembaga Kemahasiswaan Kepanitiaan</h2>
+			<table class="table">
+					<tr>
+						<th>ID Lowongan</th>
+						<td>test</td>
+					</tr>
+					<tr>
+						<th>Nama Lowongan</th>
+						<td>safsdfasds da acadsdas  sadasdasdsac sada</td>
+					</tr>
+					<tr>
+						<th>Kategori</th>
+						<td>.</td>
+					</tr>
+					<tr>
+						<th>Lingkup</th>
+						<td>.</td>
+					</tr>
+					<tr>
+						<th>Program Studi</th>
+						<td>.</td>
+					</tr>
+					<tr>
+						<th>Fakultas</th>
+						<td>.</td>
+					</tr>
+					<tr>
+						<th>Deskripsi</th>
+						<td>.</td>
+					</tr>
+					<tr>
+						<th>Kontak</th>
+						<td>.</td>
+					</tr>
+			</table>
+			<button class="tombol tombolwarna2" type="submit">Edit</button><br><br>			
+		</div>
 			
-			<div class="col-sm-3" style="background: #008080; color: white">
-				<form>
-					<input type="text" name="" placeholder="Masukkan sesuatu">
-					<select name="kategori">
-						<option value="Kepanitiaan">Kepanitiaan</option> <br>
-						<option value="UKM">UKM</option>
-						<option value="BE">BE</option>					
-					</select></br>
-					<select name="lingkup">
-						<option value="Universitas">Universitas</option>
-						<option value="Fakultas">Fakultas</option>
-						<option value="Jurusan">Jurusan</option>					
-					</select></br>
-					<input type="button" value="Cari">
-				</form>
-			</div>
-			<div class="col-sm-9">
-				<div class="col-sm-3">
-					<h2>Daftar Recruitment Lembaga Kemahasiswaan Kepanitiaan</h2>
-				</div>
-				<div class="col-sm-3">
-					<h2>Daftar Recruitment Lembaga Kemahasiswaan Kepanitiaan</h2>
-				</div>
-				<div class="col-sm-3">
-					<h2>Daftar Recruitment Lembaga Kemahasiswaan Kepanitiaan</h2>
-				</div>
-			</div>
-		</div>			
 	</div>
 </div>
-	
+
 <!--FOOTER-->
 <?php
 include "part/footer.php";
