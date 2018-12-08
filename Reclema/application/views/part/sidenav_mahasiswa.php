98603edf1cd9ff0ea21d1f9a1a564d8696fb9eac
radjfauzi <radjfauzi@gmail.com>
2018-12-08 17:05:23 +0700

    tambah sidenav_mahasiswa

---
+++ Reclema/application/views/part/sidenav_mahasiswa.php
@@ -0,0 +1,4 @@
+<div class="col-sm-3" style="background: #008080; color: white"><br>
+				<a style="font-size: 20px" href="https://www.google.com">Profil Saya</a><br>
+				<a style="font-size: 20px" href="https://www.google.co.id">Lembaga Kemahasiswaan yang saya daftar</a><br>
+</div>
