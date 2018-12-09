53926847ace68b731b0756e9701d45f0c62119b1
JohnLtor19 <johannes17001@mail.unpad.ac.id>
2018-12-09 19:23:15 +0700

    Halaman tentang kami

---
+++ Reclema/application/controllers/Tentang_kami.php
@@ -0,0 +1,6 @@
+<?php
+class Tentang_kami extends CI_Controller {
+	function index() {
+		$this->load->view("tentang_kami");
+	}
+}
---
+++ Reclema/application/views/tentang_kami.php
@@ -0,0 +1,43 @@
+<?php
+include "part/html_first.php";
+include "part/header.php";
+include "part/function_header.php";
+?>
+
+<!--MAIN-->
+<div class="container-fluid">
+	<div class="row" style="width: auto; height: auto; border: 3px solid skyblue; border-radius: 3px">
+		<div class="col-md-12">
+			<h2>1. Muhammad Fadillah Arsa</h2>
+			<p><img style="border-radius: 50%; float: left" width="200px" height="300px"src="<?php base_url()?>../content/Arsa.jpg"><span style="white-space: pre;">	</span>
+			Nama<span style="white-space: pre;">	</span>: Muhammad Fadillah Arsa<br>
+			<span style="white-space: pre;">	</span>NPM<span style="white-space: pre;">	</span>: 140810170005<br>
+			<span style="white-space: pre;">	</span>Jurusan<span style="white-space: pre;">	</span>: S-1 Teknik Informatika UNPAD<br>
+			<span style="white-space: pre;">	</span>Hobby<span style="white-space: pre;">	</span>: Ngoding, Blogging</p>
+		</div>
+	</div>
+	<div class="row" style="width: auto; height: auto; border: 3px solid skyblue; border-radius: 3px">		<div class="col-md-12">
+			<h2>2. Siradj Al-Fauzi</h2>
+			<p><img style="border-radius: 50%; float: left" width="200px" height="300px"src="<?php base_url()?>../content/Siradj.jpg">
+			<span style="white-space: pre;">	</span>Nama<span style="white-space: pre;">	</span>: Siradj Al-Fauzi<br>
+			<span style="white-space: pre;">	</span>NPM<span style="white-space: pre;">	</span>: 140810170021<br>
+			<span style="white-space: pre;">	</span>Jurusan<span style="white-space: pre;">	</span>: S-1 Teknik Informatika UNPAD<br>
+			<span style="white-space: pre;">	</span>Hobby<span style="white-space: pre;">	</span>: Ngoding, Blogging</p>
+		</div>
+	</div>
+	<div class="row" style="width: auto; height: auto; border: 3px solid skyblue; border-radius: 3px">
+		<div class="col-md-12">
+			<h2>3. Johannes Lumbantoruan</h2>
+			<p><img style="border-radius: 50%; float: left" width="200px" height="300px" src="<?php base_url()?>../content/Johannes.jpg">
+			<span style="white-space: pre;">	</span>Nama<span style="white-space: pre;">	</span>: Johannes Lumbantoruan<br>
+			<span style="white-space: pre;">	</span>NPM<span style="white-space: pre;">	</span>: 140810170043<br>
+			<span style="white-space: pre;">	</span>Jurusan<span style="white-space: pre;">	</span>: S-1 Teknik Informatika UNPAD<br>
+			<span style="white-space: pre;">	</span>Hobby<span style="white-space: pre;">	</span>: Ngoding,Main Game, Nonton Anime, Nonton Film</p>
+		</div>
+	</div>	
+</div>
+
+<!--FOOTER-->
+<?php
+include "part/footer.php";
+include "part/html_last.php";
